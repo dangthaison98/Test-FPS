3ba5dad99c9f74edbf4674abc41c0e53e91df771
dangthaison98 <dangthaison98job@gmail.com>
2025-02-05 14:37:26 +0700

    ok

---
+++ Assets/Gameplay/Scripts/Ui/Button/InvisibleButton.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvisibleButton : MonoBehaviour
+{
+    public float duration;
+    
+    public Button invisibleButton;
+    public FillAmountEffect fillAmountEffect;
+
+    private void Start()
+    {
+        fillAmountEffect.completeAction += NormalMode;
+    }
+
+    public void onClick()
+    {
+        //Ads
+        InvisibleMode();
+        fillAmountEffect.fillSettings.settings.duration = duration;
+        fillAmountEffect.Play();
+    }
+
+    private void InvisibleMode()
+    {
+        invisibleButton.interactable = false;
+    }
+
+    private void NormalMode()
+    {
+        invisibleButton.interactable = true;
+    }
+}
